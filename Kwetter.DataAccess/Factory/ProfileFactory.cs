991ba2c9c7ee66bb4f71c2c37c50fb9474aae91e
Joegoslavie <hugovaessen@gmail.com>
2021-04-25 01:23:03 +0200

    add all classes

---
+++ Kwetter.DataAccess/Factory/ProfileFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kwetter.DataAccess.Factory
+{
+    class ProfileFactory
+    {
+    }
+}
---
+++ Kwetter.DataAccess/Model/Tweet.cs
@@ -0,0 +1,55 @@
+﻿namespace Kwetter.DataAccess.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Tweet model.
+    /// </summary>
+    public class Tweet
+    {
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the username that owns the tweet.
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Gets or sets the display name.
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the content of the tweet.
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// Gets or sets the avatar url.
+        /// </summary>
+        public string AvatarUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating that the tweet is reported.
+        /// </summary>
+        public bool Reported { get; set; }
+
+        /// <summary>
+        /// Gets or sets the amount of likes this tweet has.
+        /// </summary>
+        public int Liked { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date the tweet was created.
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
