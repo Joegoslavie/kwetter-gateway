718dcf5d1b4bdc01f85fd81ecb1f5eb16747d2f1
Joegoslavie <hugovaessen@gmail.com>
2021-04-25 01:26:16 +0200

    set tweet classes

--- Kwetter.DataAccess/Factory/TweetFactory.cs
+++ Kwetter.DataAccess/Factory/TweetFactory.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Kwetter.DataAccess.Model;
+using Microservice.TweetGRPCService;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,22 @@ namespace Kwetter.DataAccess.Factory
 {
     class TweetFactory
     {
+        internal static Tweet Parse(TweetResponseData response)
+        {
+            if (response == null)
+            {
+                throw new ArgumentNullException(nameof(response));
+            }
+
+            return new Tweet
+            {
+                Id = response.Id,
+                Username = response.Username,
+                DisplayName = response.DisplayName,
+                AvatarUrl = response.AvatarUri,
+                Content = response.Description,
+                CreatedAt = new DateTime(response.CreatedAt),
+            };
+        }
     }
 }
