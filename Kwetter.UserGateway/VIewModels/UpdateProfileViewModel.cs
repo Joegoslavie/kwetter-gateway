8a876d3835880ca975b204ad41e21810b347b09a
Joegoslavie <hugovaessen@gmail.com>
2021-06-13 18:41:20 +0200

    quick push

--- Kwetter.UserGateway/VIewModels/UpdateProfileViewModel.cs
+++ Kwetter.UserGateway/VIewModels/UpdateProfileViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace Kwetter.UserGateway.VIewModels
         public string DisplayName { get; set; }
         public string WebsiteUrl { get; set; }
         public string Location { get; set; }
+
+        [StringLength(160, MinimumLength = 3, ErrorMessage = "Max 160 characters")]
         public string Description { get; set; }
     }
 }
